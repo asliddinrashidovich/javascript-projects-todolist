ba36bd04459e1d1a69790b6adbb69c766360401e
Asliddin <asliddinrashidovich7@gmail.com>
2025-02-02 11:40:55 +0500

    the todo app is finished

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const formCreate = document.getElementById('form-create')
 const formEdit = document.getElementById('form-edit')
 const listGroupTodo = document.getElementById('list-group-todo')
 const messageCreate = document.getElementById('message-create')
+const messageEdit = document.getElementById('message-edit')
 const time = document.getElementById('time')
 const modal = document.getElementById('modal')
 const overlay = document.getElementById('overlay')
@@ -14,10 +15,14 @@ const closeEl = document.getElementById('close')
 
 
 let todos = localStorage.getItem("list") ?  JSON.parse(localStorage.getItem("list")) : [];
+let openModel;
 
+// focus on input in DOMContent load
+document.addEventListener('DOMContentLoaded', ()=> {
+    formCreate['input-create'].focus()
+})
 
 // set current data and time
-
 function setTime () {
     let now = new Date();
     const months = [
@@ -45,7 +50,6 @@ setInterval(()=> {
     setTime()
 }, 1000)
 
-
 if (todos.length) setItemOfTodos()
 
 // set todos
@@ -53,18 +57,17 @@ function setTodosLocalStorage() {
     localStorage.setItem('list', JSON.stringify(todos))
 }
 
-
 // set Item 
 function setItemOfTodos() {
     const todos =  JSON.parse(localStorage.getItem("list"));
-    listGroupTodo.innerHTML = ' '
+    listGroupTodo.innerHTML = ''
     todos.forEach((item, i) => {
         listGroupTodo.innerHTML += `
             <li ondblclick="markTodos(${i})"  class="list-group-item d-flex  ${item.complated == true ? 'complated' : ''} justify-content-between">
               ${item.text}
               <div class="todo-icon">
                 <span class="opacity-50 me-2">${item.time}</span>
-                <img   src="img/edit.svg" alt="edit ocon" width="25" height="25">
+                <img onclick="editTodos(${i})"  src="img/edit.svg" alt="edit ocon" width="25" height="25">
                 <img onclick="deleteTodos(${i})" src="img/delete.svg" alt="edit ocon" width="25" height="25">
               </div>
             </li>
@@ -80,32 +83,26 @@ formCreate.addEventListener('submit', (e)=>{
     
     if(inputValue.length) {
         todos.push({text: inputValue, time: setTime(), complated: false})
-        console.log(todos)
         setTodosLocalStorage()
         setItemOfTodos()
-        
     } else {
-        messageCreate.innerHTML = 'Please enter some text'
-        setTimeout(() => {
-            messageCreate.innerHTML = ''
-        }, 2500);
+        errorMessage(messageCreate, 'Please enter some text')
     }
 })
 
 
 // delete items
-
 function deleteTodos(id) {
     const delTodos =  todos.filter((item, i) => {
         return id !== i
     })
-
     todos = delTodos;
-    console.log(todos)
     setTodosLocalStorage()
     setItemOfTodos()
 }
 
+
+// duble Click todos
 function markTodos(id) {
     const markedTodos = todos.map((item, i) => {
         if(id == i) {
@@ -118,13 +115,61 @@ function markTodos(id) {
     todos = markedTodos;
     setTodosLocalStorage()
     setItemOfTodos()
-    console.log('asliddin')
+}
+
+// Edit Todos 
+function editTodos(id) {
+    openModelFunc()
+    openModel = id; 
+}
+
+formEdit.addEventListener('submit', (e)=> {
+    e.preventDefault() 
+    let editInput = formEdit['input-edit'].value
+
+    const editElements = todos.map((item, i) => {
+        if(openModel == i && editInput.length) {
+            closeModel()
+            return  {...item,  text:  editInput, time: setTime()}
+        } else if (openModel == i && editInput.length == 0) {
+            errorMessage(messageEdit, 'Please enter some text')
+            return {...item,}
+        } else {
+            return {...item,}
+        }
+    })
+
+    todos = editElements
+    setTodosLocalStorage()
+    setItemOfTodos()
+    formEdit.reset()
+})
+
+// key up events
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'Escape')  closeModel()
+})
+
+overlay.addEventListener('click', closeModel)
+closeEl.addEventListener('click', closeModel)
+
+// close model function
+function closeModel() {
+    overlay.classList.add('hidden')
+    modal.classList.add('hidden')
+}
+
+// open model function
+function openModelFunc() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
 }
 
 
-
-
-
-
-
-
+// Error message function
+function errorMessage(element, text) {
+    element.innerHTML = text;
+    setTimeout(() => {
+        element.innerHTML = '';
+    }, 2500)
+}
